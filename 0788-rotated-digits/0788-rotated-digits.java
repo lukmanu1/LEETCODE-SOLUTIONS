969717b8970e9f85cd40ad8cb0e0fd0688f31e77
LUKMANKM <145091843+lukmanu1@users.noreply.github.com>
2026-05-02 16:55:02 +0530

    Time: 6 ms (30.77%), Space: 42.3 MB (44.8%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,11 +1,51 @@
 class Solution {
     public int rotatedDigits(int n) {
-        Boolean dp[] = new Boolean[n + 1];
-        dp[0] = false;
-        dp[0] = false;
-        dp[0] = false;
-        dp[0] = false;
-        dp[0] = false;
-        dp[0] = false;
+        int dp[] = new int[n + 2];
+
+        int count = 0, a = 0, b = 0;
+
+        for(int i = 0; i<=n; i++){
+            if( i == 0 ||
+                i == 1 ||
+                i == 8 )
+            {
+                dp[i] = 1;
+                continue;
+            }
+
+            if(
+                i == 2 || 
+                i == 5 ||
+                i == 6 ||
+                i == 9 
+            ){
+                dp[i] = 2;
+                count++;
+                continue;
+            }
+
+            if(
+                i == 3 ||
+                i == 4 ||
+                i == 7
+            ){
+                dp[i] = 0;
+                continue;
+            }
+
+            a = i / 10;
+            b = i % 10;
+
+            if(dp[a] == 1 && dp[b] == 1)dp[i] = 1;
+            else if(dp[a] >= 1 && dp[b] >= 1){
+                dp[i] = 2;
+                count++;
+            }
+            else dp[i] = 0;
+
+
+        }
+
+        return count;
     }
 }
