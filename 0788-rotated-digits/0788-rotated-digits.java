b8d4c69109db1f9244191e38448bda9fe400950e
LUKMANKM <145091843+lukmanu1@users.noreply.github.com>
2026-05-02 16:35:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,31 +1,11 @@
 class Solution {
-    public int findNumberOfValidDigit(int num){
-        int digit = 0, count = 0;
-        while(num > 0){
-            digit = num % 10;
-            num /= 10;
-
-            if( digit == 2 ||
-                digit == 5 ||
-                digit == 6 ||
-                digit == 9
-            ) count++;
-
-            if( digit == 3 ||
-                digit == 4 ||
-                digit == 7
-                ) return 0;
-        }
-
-        return count > 0 ? 1 : 0;
-    }
     public int rotatedDigits(int n) {
-        int sum = 0;
-        int count = 0;
-        for(int i = 2; i<=n; i++){
-            sum += findNumberOfValidDigit(i);
-        }
-
-        return sum;
+        Boolean dp[] = new Boolean[n + 1];
+        dp[0] = false;
+        dp[0] = false;
+        dp[0] = false;
+        dp[0] = false;
+        dp[0] = false;
+        dp[0] = false;
     }
 }
