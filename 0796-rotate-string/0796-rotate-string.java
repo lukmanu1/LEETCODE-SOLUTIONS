c5af904d1abfac33edf0b4516d2b909008827d4a
LUKMANKM <145091843+lukmanu1@users.noreply.github.com>
2026-05-03 12:03:31 +0530

    Time: 2 ms (9.61%), Space: 42.7 MB (73.32%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,15 +1,22 @@
 class Solution {
+    public boolean compare_from_i_th_char(String s, int start, String goal){
+        for(int i = start, j = 0; i < s.length() + start; i++, j++){
+            if(s.charAt(i % s.length()) != goal.charAt(j)) return false;
+        }
+
+        return true;
+    }
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length())return false;
-        String str=s+s;
-        System.out.print(str);
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)==goal.charAt(0)){
-                if (i + goal.length() <= str.length() && str.substring(i, i + goal.length()).equals(goal)) {
-    return true;
-}
+        if(s.length() != goal.length())return false;
+        char startChar = goal.charAt(0);
+
+        for(int i = 0; i< s.length(); i++){
+            if(s.charAt(i) == startChar){
+                System.out.println("in here "+i);
+                if(compare_from_i_th_char(s, i, goal)) return true;
             }
         }
+
         return false;
     }
 }
