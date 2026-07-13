fbe833b58b28f3be1e8fe24b579efe539a1cf22b
LUKMANKM <145091843+lukmanu1@users.noreply.github.com>
2026-07-13 12:53:49 +0530

    Time: 0 ms (100%), Space: 48.6 MB (69.57%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,35 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j=0;
-        int i=0;
-        int val = nums[0];
-        int n = nums.length;
-        int count = 0;
-        while(j<nums.length){
-            while(j<n && nums[j] == val){
+        int i = 2, j = 1;
+        for(i = 2; i<nums.length; i++){
+            if(nums[j - 1] != nums[i]){
                 j++;
-                count++;
-            }
-
-            if(j<n && count>2){
-                nums[i]=val;
-                nums[i+1]=val;
-                i+=2;
-                val = nums[j];
-                count = 0;
-                System.out.println("hello "+i+" "+val+" "+j);
-            }else {
-                nums[i] = val;
-                if(count >= 2)nums[i+1] = val;
-                //if(j==n)break;
-                if(j<n) val = nums[j];
-                else break;
-                i+=count;
-                count = 0;
-                System.out.println("hey "+i+" "+j);
+                nums[j] = nums[i];
             }
         }
-        int ans = count <= 2 ? i + count : i + 2;
-        return ans;
+
+        return j + 1;
     }
 }
